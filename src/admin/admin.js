e1d82bf1b4dce4028679363f6c1c93b029607cd2
longlei <longlei_2012@163.com>
2017-04-06 21:30:49 +0800

    修改用户信息

--- src/admin/admin.js
+++ src/admin/admin.js
@@ -262,10 +262,102 @@ define(['jquery','adminTemplate','common','bootstrap','bootstrapValidator','boot
         
     })
 
+    //编辑记录
+    $('#btn_edit').click(function(){
+        var selectIndex = $('input[name="btSelectItem"]:checked ').parent().next().text();
+        if(selectIndex){
+            $.ajax({
+                url:"http://120.27.224.143:10010/v1/admin/user/"+selectIndex+"/getDetail",
+                type:"get",
+                dataType:'json',
+                timeout:60000,
+                headers:{
+                        'SUPERADMIN-API-KEY' : JSON.parse(localStorage.getItem("session")).apiKey
+                },
+                success:function(data){
+                    common.htmlModule({list:$.extend({},data.data,zoneData)},$('#userInfoControl')[0],adminTemplate);
+                    console.log($.extend({},data.data,zoneData));
+                    $('#userInfoControl').modal('show');
+                    ValidatorInit();
+                    $('#selectpicker1').selectpicker({
+                            style: 'btn-default',
+                            size: 4,
+                            liveSearch:true
+                    });
+                    $('#selectpicker2').selectpicker({
+                            style: 'btn-default',
+                            size: 4,
+                            liveSearch:true
+                    });
+                    var selectuserGroup = $('#selectpicker1 option').filter(function(index){
+                        return $(this).attr('data-id') ==data.data.userGroup
+                    });
+                        $('#selectpicker1').selectpicker('val',selectuserGroup.text())
+                        $('#selectpicker2').selectpicker('val',data.data.address)
+                    
+                    $("#save_btn").click(function(){ 
+                        $('.form-horizontal').bootstrapValidator('validate');
+                        sexval = $('input:radio').filter(function(i){
+                            return ($(this).attr('name') == 'sex' && $(this)[0].checked)
+                        })
+                        var data = {
+                            username:$('#username').val(),
+                            englishName:$('#englishName').val(),
+                            gender:sexval.val(),
+                            userGroup:$('#selectpicker1 option:selected').attr('data-id'),
+                            mobile:$('#phone').val(),
+                            belongingZoneId:$('#selectpicker2 option:selected').attr('data-id'),
+                            belongingZone:$('#selectpicker2').selectpicker('val'),
+                            email:$('#email').val()
+                        }
+                        
+                        if($('.form-horizontal').data('bootstrapValidator').isValid()){
+                                var selectIndex = $('input[name="btSelectItem"]:checked ').parent().next().text();
+                                if(selectIndex){
+                                    $.ajax({
 
+                                        url:'http://120.27.224.143:10010/v1/admin/user/'+selectIndex+'/modify',
+                                        type:"post",
+                                        dataType:'json',
+                                        timeout:60000,
+                                        headers:{
+                                            "Content-Type": 'application/json',
+                                            'SUPERADMIN-API-KEY' : JSON.parse(localStorage.getItem("session")).apiKey
+                                        },
+                                        data:JSON.stringify(data),
+                                        success:function(data){
+                                            $('#table').bootstrapTable('refresh');
+                                            $('#alert-success').show();
+                                            setTimeout(function(){
+                                                $('#alert-success').hide()
+                                                $('#userInfoControl').modal('hide')
+                                            },1000)
+                                        }
+                                        ,
+                                        error:function(XMLHttpRequest, textStatus, errorThrown){
+                                            $('#alert-danger').show()
+                                            setTimeout(function(){
+                                                $('#alert-danger').hide()
+                                            },1000)
+                                        }
+                                    })
+                                }else{
+                                    alert('请选中一行记录')
+                                }
+                        }
+                    })
+                }
+                ,
+                error:function(XMLHttpRequest, textStatus, errorThrown){
+                }
+            })
+        }else{
+            alert('请选中一行记录')
+        }
+    })
     //保存以及修改的方法
     function modiAdd(){
-        sexval = $('input:radio').filter(function(i){
+            sexval = $('input:radio').filter(function(i){
                 return ($(this).attr('name') == 'sex' && $(this)[0].checked)
             })
             var data = {
@@ -280,7 +372,6 @@ define(['jquery','adminTemplate','common','bootstrap','bootstrapValidator','boot
                 email:$('#email').val()
             }
             
-            $('.form-horizontal').bootstrapValidator('validate');
             if($('.form-horizontal').data('bootstrapValidator').isValid()){
                 $.ajax({
                     url:'http://120.27.224.143:10010/v1/admin/user/add',
@@ -311,29 +402,47 @@ define(['jquery','adminTemplate','common','bootstrap','bootstrapValidator','boot
             }
     }
     //模态框加载事件，包括了新增以及修改以及查看
-    $('#userInfoControl').on('show.bs.modal', function (e) {
-        var triggerBtn = $(e.relatedTarget);
-        // if(triggerBtn.attr('id')=='btn_add'){
-            
-        // }
-        // if(triggerBtn.attr('id')=='btn_edit'){
-            
-        // }
-        common.htmlModule({list:zoneData},$('#userInfoControl')[0],adminTemplate);
-        ValidatorInit();
-        $('#selectpicker1').selectpicker({
-                style: 'btn-default',
-                size: 4,
-                liveSearch:true
-        });
-        $('#selectpicker2').selectpicker({
-                style: 'btn-default',
-                size: 4,
-                liveSearch:true
-        });
-         //点击保存按钮的回调
-        $("#save_btn").click(function(){ 
-           modiAdd();
-        })
+    $('#btn_add').click(function(){
+         common.htmlModule({list:zoneData},$('#userInfoControl')[0],adminTemplate);
+         
+            ValidatorInit();
+            $('#selectpicker1').selectpicker({
+                    style: 'btn-default',
+                    size: 4,
+                    liveSearch:true
+            });
+            $('#selectpicker2').selectpicker({
+                    style: 'btn-default',
+                    size: 4,
+                    liveSearch:true
+            });
+            //点击保存按钮的回调
+            $("#save_btn").click(function(){ 
+                $('.form-horizontal').bootstrapValidator('validate');
+                modiAdd();
+            })
+            $('#userInfoControl').modal('show');
     })
+    // $('#userInfoControl').on('show.bs.modal', function (e) {
+    //     var triggerBtn = $(e.relatedTarget);
+        
+    //     common.htmlModule({list:zoneData},$('#userInfoControl')[0],adminTemplate);
+    //     ValidatorInit();
+    //     $('#selectpicker1').selectpicker({
+    //             style: 'btn-default',
+    //             size: 4,
+    //             liveSearch:true
+    //     });
+    //     $('#selectpicker2').selectpicker({
+    //             style: 'btn-default',
+    //             size: 4,
+    //             liveSearch:true
+    //     });
+    //     //点击保存按钮的回调
+    //     $("#save_btn").click(function(){ 
+    //         $('.form-horizontal').bootstrapValidator('validate');
+    //         modiAdd();
+    //     })
+        
+    // })
 });
