7417b96fa10ca56b689c21c2331ad9e2ad836110
mangdehuang <zhuerwei3320213>
2017-05-25 06:53:45 +0800

    获取单词重构

--- src/lessons/game.js
+++ src/lessons/game.js
@@ -1,146 +1,159 @@
 require.config({
-    paths : {
-        "jquery" : "../js/libs/jquery.min",
-        "common":'../js/common'
-    }
-    ,
-    shim: {
-         'common':{
-            exports: 'common'
-         }
-　　　　}
+	paths: {
+		"jquery": "../js/libs/jquery.min",
+		"common": '../js/common'
+	},
+	shim: {
+		'common': {
+			exports: 'common'
+		}　　　　
+	}
 })
-define(['jquery','common'],function($,common){
-	$(function(){
-	var ofenshu = document.getElementById("fenshu");
-	var odaojishi = document.getElementById("daojishi");
-	var yinyue = document.getElementById("yinyue");
-	var wordaudio = document.getElementById("wordaudio");//单词播放
-	var Btn = document.getElementById("btn");
-	var audio = document.getElementById("audio");
-	var part = document.getElementById("part");
-	var integral = document.getElementById("integral");
-	var fenshu = 0;
-	var next = document.getElementById("next");
-	var daojishi = 60;
-	var appleMain = document.getElementById("appleMain");
-	var currentpart = document.getElementById('currentpart');
-	var getUrl = "http://120.27.224.143:10010";
-	var  unit = "2";		
-		 var　page = "1";
-	var options = {
-		part : 0,
-		integraljifen:0,
-		currentpart:1,
-		total:2000000
-	}
-	var session = {};
-	if(localStorage.session){ 
-		session = JSON.parse(localStorage.session);
-	}	
-	var dataArray  = [
-		{
-			wordList:['zhangsan','lisi','wangwu','zhaoliu','heihei'],
-			audio:'../images/ballon_game/0.mp3',
-			targetWord:'zhangsan'
+define(['jquery', 'common'], function($, common) {
+	$(function() {
+		var ofenshu = document.getElementById("fenshu");
+		var odaojishi = document.getElementById("daojishi");
+		var yinyue = document.getElementById("yinyue");
+		var wordaudio = document.getElementById("wordaudio"); //单词播放
+		var Btn = document.getElementById("btn");
+		var audio = document.getElementById("audio");
+		var part = document.getElementById("part");
+		var integral = document.getElementById("integral");
+		var fenshu = 0;
+		var next = document.getElementById("next");
+		var daojishi = 60;
+		var appleMain = document.getElementById("appleMain");
+		var currentpart = document.getElementById('currentpart');
+		var getUrl = "http://120.27.224.143:10010";
+		var unit = "1";
+		var　 page = "1";
+		var options = {
+			part: 0,
+			integraljifen: 0,
+			currentpart: 1,
+			total: 2000000
 		}
-		,
-		{
-			wordList:['heihei','haha','gaga','hehe','gg'],
-			audio:'../images/ballon_game/0.mp3',
-			targetWord:'haha'
+		var session = {};
+		if (localStorage.session) {
+			session = JSON.parse(localStorage.session);
 		}
-	]
+		var wordList = []; //记录第一次获取到的所有单词
+		var nowIndex = 0; //当前是开始选择的那个单词
+		var dataArray = [{
+			wordList: ['zhangsan', 'lisi', 'wangwu', 'zhaoliu', 'heihei'],
+			audio: '../images/ballon_game/0.mp3',
+			targetWord: 'zhangsan'
+		}, {
+			wordList: ['heihei', 'haha', 'gaga', 'hehe', 'gg'],
+			audio: '../images/ballon_game/0.mp3',
+			targetWord: 'haha'
+		}]
 
 
-	Btn.onclick = function () {
-		$(next).hide();
-		Btn.style.display = 'none';
-		audio.style.display = 'inline-block';
-		part.style.display = 'none';
-		integral.style.display = 'none';
-		appleMain.style.display = 'block';
-		showWords();
-	}
-	$(".appleItem").click(function(){
-		var thisHtml = $(this).text();
-		//console.log(thisHtml);
-		if(thisHtml == dataArray[options.part].targetWord){
-			options.integraljifen += 10;
-			$(integral).html(options.integraljifen);
-			$(next).show();
-			//alert('回答正确，加10分');
-		}else{
-			alert('回答错误')
-			options.integraljifen -= 10;
-			if(options.integraljifen<=0){
-				options.integraljifen =0
+		Btn.onclick = function() {
+			$(next).hide();
+			Btn.style.display = 'none';
+			audio.style.display = 'inline-block';
+			part.style.display = 'none';
+			integral.style.display = 'none';
+			appleMain.style.display = 'block';
+			showWords();
+		}
+		$(".appleItem").click(function() {
+			var thisHtml = $(this).text();
+			//console.log(thisHtml);
+			if (thisHtml == dataArray[options.part].targetWord) {
+				options.integraljifen += 10;
+				$(integral).html(options.integraljifen);
+				$(next).show();
+				//alert('回答正确，加10分');
+			} else {
+				alert('回答错误')
+				options.integraljifen -= 10;
+				if (options.integraljifen <= 0) {
+					options.integraljifen = 0
+				}
+				$(integral).html(options.integraljifen);
 			}
-			$(integral).html(options.integraljifen);
+
+		})
+		$(next).click(function() {
+			if (options.currentpart == options.total) {
+				alert('游戏已经结束');
+				return;
+			}
+			//options.part +=1;
+			options.currentpart += 1;
+			$(currentpart).html(options.currentpart);
+			if(wordList.length-1 == nowIndex){ 
+				alert("游戏结束");
+				return false;
+			}
+			showWords1(wordList[nowIndex+1],nowIndex+1);
+		})
+
+		function showWords() {
+
+			$.ajax({
+				url: getUrl + '/v1/unit/' + unit + '/game/page/' + page + '/get',
+				type: "get",
+				dataType: 'json',
+				timeout: 60000,
+				headers: {
+					"Content-Type": 'application/json',
+					'SUPERADMIN-API-KEY': session.apiKey
+				},
+				success: function(data) {
+					wordList = data.data.componentGroups[0].components;
+					if (wordList.length > 0) {
+						showWords1(wordList[0], 0);
+					}
+
+				},
+				error: function() {
+					alert("获取数据异常");
+				}
+			});
 		}
-		
-	})
-	$(next).click(function(){
-		if(options.currentpart == options.total){
-			alert('游戏已经结束');
-			return;
+
+		function showWords1(wordObj, index) {
+			nowIndex = index;
+			var refpositon = null;
+			words = wordObj.resources;
+			if (words) {
+				dataArray[options.part].wordList = [];
+				for (var i = 0; i < words.length; i++) { //根据position获取到正确等
+					if (words[i].type == "sound") {
+						refpositon = words[i].refPosition;
+						wordaudio.src = getUrl + words[i].content;
+					} else {
+						if (refpositon == words[i].position) {
+							dataArray[options.part].targetWord = words[i].content;
+							dataArray[0].targetWord = words[1].content;
+						}
+						//correctWord = words[i];
+						dataArray[options.part].wordList.push(words[i].content);
+					}
+				}
+
+			}
+
+			dataArray[options.part].wordList.forEach(function(value, index, array) {
+				$(appleMain).find('span').eq(index).html(value);
+			});
+			wordaudio.load();
+			wordaudio.play();
 		}
-		//options.part +=1;
-		options.currentpart+=1;
-		$(currentpart).html(options.currentpart);
-		showWords();		
-	})
-   
-    function showWords (){ 		 
-		 var refpositon = null;
-     	  $.ajax({
-                url:getUrl+'/v1/unit/'+unit+'/game/page/'+page+'/get',
-                type:"get",
-                dataType:'json',
-                timeout:60000,
-                headers:{"Content-Type": 'application/json','SUPERADMIN-API-KEY': session.apiKey},                
-                success:function(data){                   
-                	 words = data.data.componentGroups[0].components[0].resources;
-				    	if(words){ 
-				    		dataArray[options.part].wordList = [];
-							for(var i=0;i < words.length;i++ ){ //根据position获取到正确等
-								if (words[i].type == "sound") { 
-									refpositon = words[i].refPosition;									
-									wordaudio.src = getUrl+words[i].content;									
-								}else{ 
-									if(refpositon == words[i].position){ 
- 										dataArray[options.part].targetWord = words[i].content; 										
-				     					dataArray[0].targetWord = words[1].content;
-									}
-									//correctWord = words[i];
-									dataArray[options.part].wordList.push(words[i].content);
-								}
-							} 
-							 
-				     	} 
 
-				     	dataArray[options.part].wordList.forEach(function(value, index, array){
-							$(appleMain).find('span').eq(index).html(value);
-						});
-				       wordaudio.load();
-					    wordaudio.play();				  
-                },
-                error:function(){ 
-                	alert("获取数据异常");
-                }
-            });  
-    }
+		audio.onclick = function() {
+			wordaudio.load();
+			wordaudio.play();
+		}
 
-    audio.onclick = function(){ 
-    	wordaudio.load();
-    	 wordaudio.play();		
-    } 
-
-    $(".close").on("click",function(){ 
-           location.href = getUrl + "/lessons/unit.html#"+unit;
-	});
+		$(".close").on("click", function() {
+			location.href = getUrl + "/lessons/unit.html#" + unit;
+		});
 
 	})
-    
+
 })
-    
--- src/lessons/mouse_game.js
+++ src/lessons/mouse_game.js
@@ -1,233 +1,247 @@
 require.config({
-    paths : {
-        "jquery" : "../js/libs/jquery.min",
-        "moduleHtml":'../js/common'
-    }
-    ,
-    shim: {
-         'moduleHtml':{
-            exports: 'template'
-         }
-　　　}
+	paths: {
+		"jquery": "../js/libs/jquery.min",
+		"moduleHtml": '../js/common'
+	},
+	shim: {
+		'moduleHtml': {
+			exports: 'template'
+		}　　　
+	}
 })
- define(['jquery','moduleHtml'],function($,template){
- 	//模拟参数
- 	var getUrl = "http://120.27.224.143:10010";
- 	var unit = "1";
- 	var page = '1';
+define(['jquery', 'moduleHtml'], function($, template) {
+	//模拟参数
+	var getUrl = "http://120.27.224.143:10010";
+	var unit = "1";
+	var page = '1';
 
 
-     var score = 0;
-     var mouses = $('#table  .mouse_content');
-     var $audio = $("#yinyue");
-     var $audioImg = $("#audio");
-     var yinyueDom = document.getElementById("yinyue");	
-     var time1;
-     var session = {};
-     if(localStorage.session){ 
+	var score = 0;
+	var mouses = $('#table  .mouse_content');
+	var $audio = $("#yinyue");
+	var $audioImg = $("#audio");
+	var yinyueDom = document.getElementById("yinyue");
+	var time1;
+	var session = {};
+	if (localStorage.session) {
 		session = JSON.parse(localStorage.session);
 	}
-     
-     var constant = {
-         holeNum:10,//洞个数
-         showInterval:4000,//出现时间间隔
-         hideInterval:8300,//出现多长时间后 隐藏
-         totalScore:10000000,//总分上限
-         timeScore:10,//每次积分
-         timeWordsNum:3,//每次出现的单词的个数
-         isStart:false,
-     };
-     var correctWord = null;
+	var wordList = []; //记录第一次获取到的所有单词
+	var nowIndex = 0; //当前是开始选择的那个单词
+	var constant = {
+		holeNum: 10, //洞个数
+		showInterval: 4000, //出现时间间隔
+		hideInterval: 8300, //出现多长时间后 隐藏
+		totalScore: 10000000, //总分上限
+		timeScore: 10, //每次积分
+		timeWordsNum: 3, //每次出现的单词的个数
+		isStart: false,
+	};
+	var correctWord = null;
 
 
-     // 1.跳出一个地鼠，过一会儿隐藏
-     function show(word){     
+	// 1.跳出一个地鼠，过一会儿隐藏
+	function show(word) {
 
-         var randNum = Math.random() * constant.holeNum;
-         var a = Math.floor(randNum);
-         var mouse = mouses.get(a);
+		var randNum = Math.random() * constant.holeNum;
+		var a = Math.floor(randNum);
+		var mouse = mouses.get(a);
 
-         if(!_show($(mouse),word)){
-             show(word);
-         }
+		if (!_show($(mouse), word)) {
+			show(word);
+		}
 
-         function _hide(){
-             $(mouse).addClass('mouseDown').removeClass('mouseUp');
-             $(mouse).find("img").addClass('mouseDown').removeClass('mouseUp');
-             $(mouse).find("span").hide();
-         }
-         // setTimeout() 方法用于在指定的毫秒数后调用函数或计算表达式
-         // time = setTimeout(_hide, 5500);
-         // return time
-     }
+		function _hide() {
+			$(mouse).addClass('mouseDown').removeClass('mouseUp');
+			$(mouse).find("img").addClass('mouseDown').removeClass('mouseUp');
+			$(mouse).find("span").hide();
+		}
+		// setTimeout() 方法用于在指定的毫秒数后调用函数或计算表达式
+		// time = setTimeout(_hide, 5500);
+		// return time
+	}
 
-     function _hide($mouse_content) {
-         if($mouse_content.height() == 0) return false;
-         $mouse_content.addClass('mouseDown').removeClass('mouseUp');
-         $mouse_content.find("img").addClass('mouseDown').removeClass('mouseUp');
-         $mouse_content.find("span").hide();
-     }
+	function _hide($mouse_content) {
+		if ($mouse_content.height() == 0) return false;
+		$mouse_content.addClass('mouseDown').removeClass('mouseUp');
+		$mouse_content.find("img").addClass('mouseDown').removeClass('mouseUp');
+		$mouse_content.find("span").hide();
+	}
 
-     function _show($mouse_content,word){
-         if($mouse_content.hasClass("mouseUp")) return false;
-         $mouse_content.addClass('mouseUp').removeClass('mouseDown');
-         $mouse_content.find("img").addClass('mouseUp').removeClass('mouseDown');
-         $mouse_content.find("span").text(word).show();
-         return true;
-     };
-     
-     function _hideAllHole() {
-         if(mouses.length > 0){
-            for(var i =0;i < mouses.length;i ++){
-                _hide($(mouses[i]));
-            }
-         }
-     }
+	function _show($mouse_content, word) {
+		if ($mouse_content.hasClass("mouseUp")) return false;
+		$mouse_content.addClass('mouseUp').removeClass('mouseDown');
+		$mouse_content.find("img").addClass('mouseUp').removeClass('mouseDown');
+		$mouse_content.find("span").text(word).show();
+		return true;
+	};
+
+	function _hideAllHole() {
+		if (mouses.length > 0) {
+			for (var i = 0; i < mouses.length; i++) {
+				_hide($(mouses[i]));
+			}
+		}
+	}
 
 
 
-     //2.获取单词或者句子
-     function _getWord(){
-         //通过ajax  TODO
-         var param = {};         
-         var words = ["get","input","words","acc","add","floor","mouse"];
-         var word = words[template.getRandom(0,words.length)];
-         return word;
-     }
+	//2.获取单词或者句子
+	function _getWord() {
+		//通过ajax  
+		var param = {};
+		var words = ["get", "input", "words", "acc", "add", "floor", "mouse"];
+		var word = words[template.getRandom(0, words.length)];
+		return word;
+	}
 
-     function _getWrongWord(word){
-        var ns = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n'];
-        var index = template.getRandom(0,word.length);
-        var o = word.charAt(index);
-        var result = word.replace(o,ns[template.getRandom(0,ns.length)]);
-        // console.log(result);
-        return result;
-     }
- 
-     // 3.跳出一批地鼠
-     function play(){
-     	var  words = null;
-     	var  showwords = [];
-     	  $.ajax({
-                url:getUrl+'/v1/unit/'+unit+'/game/page/'+page+'/get',
-                type:"get",
-                dataType:'json',
-                timeout:60000,
-                headers:{"Content-Type": 'application/json','SUPERADMIN-API-KEY': session.apiKey},               
-                success:function(data){   
-                	var refpositon = null;                   	      
-                	 words = data.data.componentGroups[0].components[0].resources;
-				    	if(words){ 
-							for(var i=0;i < words.length;i++ ){ //根据position获取到正确等
-								if (words[i].type == "sound") { 
-									refpositon = words[i].refPosition;									
-									yinyueDom.src = getUrl+words[i].content;																
-								}else{ 
-									if(refpositon == words[i].position){ 
- 										correctWord = words[i].content;
-									} 
-									showwords.push(words[i].content);
-								}
-							} 							
-					         var t1 = show(showwords[0]);
-					         var t2 = show(showwords[1]);
-					         var t3 = show(showwords[2]);
-					         yinyueDom.load();
-					         yinyueDom.play();
-				     	}
-                },
-                error:function(){ 
-                	alert("获取数据异常");
-                }
-            });     	
-     }
+	function _getWrongWord(word) {
+		var ns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'];
+		var index = template.getRandom(0, word.length);
+		var o = word.charAt(index);
+		var result = word.replace(o, ns[template.getRandom(0, ns.length)]);
+		// console.log(result);
+		return result;
+	}
 
-     //4.判断正确与否  然后算分 还是减分
-     function isCorrect(word){
-        return word == correctWord ? true:false;
-     }
+	// 3.跳出一批地鼠
+	function play() {
+		$.ajax({
+			url: getUrl + '/v1/unit/' + unit + '/game/page/' + page + '/get',
+			type: "get",
+			dataType: 'json',
+			timeout: 60000,
+			headers: {
+				"Content-Type": 'application/json',
+				'SUPERADMIN-API-KEY': session.apiKey
+			},
+			success: function(data) {				
+				wordList = data.data.componentGroups[0].components;
+				if (wordList.length > 0) {
+					play1(wordList[0], 0);
+				}
+			},
+			error: function() {
+				alert("获取数据异常");
+			}
+		});
+	}
 
-     // 5.每隔一段时间跳出一批地鼠
-     // time1 = setInterval(play, constant.showInterval);
-    
+	function play1(wordObj, index) { 
+		nowIndex = index;
+		var showwords = [];
+		var refpositon = null;
+		var words = null;
+		words = wordObj.resources;
+		if (words) {
+			for (var i = 0; i < words.length; i++) { //根据position获取到正确等
+				if (words[i].type == "sound") {
+					refpositon = words[i].refPosition;
+					yinyueDom.src = getUrl + words[i].content;
+				} else {
+					if (refpositon == words[i].position) {
+						correctWord = words[i].content;
+					}
+					showwords.push(words[i].content);
+				}
+			}
+			var t1 = show(showwords[0]);
+			var t2 = show(showwords[1]);
+			var t3 = show(showwords[2]);
+			yinyueDom.load();
+			yinyueDom.play();
+		}
+	}
 
-     //开始游戏
-    $("#startBtn").on("click",function(){
-    	$(this).hide();
+	//4.判断正确与否  然后算分 还是减分
+	function isCorrect(word) {
+		return word == correctWord ? true : false;
+	}
+
+	// 5.每隔一段时间跳出一批地鼠
+	// time1 = setInterval(play, constant.showInterval);
+
+
+	//开始游戏
+	$("#startBtn").on("click", function() {
+		$(this).hide();
 		play();
-		 $("body").css('cursor', 'url(../images/mouse_game/cursor.ico), default');
-		 constant.isStart =true;	 
-		 return false;
-    });
-
-     //下个游戏
-     $("#nextGameBtn").on("click",function(){
-          location.href = "ballon_game.html";
-     });
-     
-     // 打中地鼠
-     $('.mouse_content').click(function(){     	
-     	if(!$(this).hasClass("mouseUp")) return false;
-         var $span = $(this).find("span");
-         $(this).find("img").addClass('mouseDown').removeClass('mouseUp');
-         $span.hide();
-         _hideAllHole();         
-         if($span.text()){
-             if(isCorrect($span.text())){
-                 score += constant.timeScore;
-                 // 设置id=hit标签的src属性值为hit.wav并播放它
-         		$('#hit').attr('src', '../music/mouse_game/hit.wav').get(0).play();
-                 setTimeout(function () {
-					 if(score < 100){
-							play();
-					 }                     
-                 },300);
-             }else{
-                alert("选错了！");
-                if(score>0){ 
-					score -= constant.timeScore;
-                }
-                
-                setTimeout(function () {
-                    var t1 = show(correctWord);
-                    var t2 = show(_getWrongWord(correctWord));
-                    var t3 = show(_getWrongWord(correctWord));
-                },300);
-             }
-         }
-
-         //TODO
-         // 设置id=score的标签的文本内容(在这里指的是p标签)
-         $('#score').text('得分：' + score);
-         $("body").css('cursor', 'url(../images/mouse_game/cursor-down.ico), default');
-         setTimeout(function () {
-             $("body").trigger("mouseup");
-         },250);
-         if(score>=constant.totalScore){
-			 $("#nextGameBtn").show();
-			 $("#startBtn").hide();
-			 _hideAllHole(); 
-             clearInterval(time1);
-             alert('游戏结束');
-             return
-         }
-         
-         return false;
-     });
-
-    //播放
-	$audioImg.on("click",function(){ 
-		yinyueDom.play();
-	}); 
-
-     // 鼠标没有被按下时，指针图片改为image/cursor.ico
-     $('body').mouseup(function(){    
-     	if(constant.isStart) { 
-     		$(this).css('cursor', 'url(../images/mouse_game/cursor.ico), default');
-     	}         
-     });
-
-     $(".close").on("click",function(){ 
-          location.href = getUrl + "/lessons/unit.html#"+unit;
+		$("body").css('cursor', 'url(../images/mouse_game/cursor.ico), default');
+		constant.isStart = true;
+		return false;
 	});
- })
+
+	//下个游戏
+	$("#nextGameBtn").on("click", function() {
+		location.href = "ballon_game.html";
+	});
+
+	// 打中地鼠
+	$('.mouse_content').click(function() {
+		if (!$(this).hasClass("mouseUp")) return false;
+		var $span = $(this).find("span");
+		$(this).find("img").addClass('mouseDown').removeClass('mouseUp');
+		$span.hide();
+		_hideAllHole();
+		if ($span.text()) {
+			if (isCorrect($span.text())) {
+				score += constant.timeScore;
+				// 设置id=hit标签的src属性值为hit.wav并播放它
+				$('#hit').attr('src', '../music/mouse_game/hit.wav').get(0).play();
+				setTimeout(function() {
+					if (nowIndex == (wordList.length-1)) {
+						alert('游戏结束');
+						//TODO 更新游戏进度
+						return false;
+					}
+					play1(wordList[nowIndex + 1], nowIndex + 1);
+				}, 300);
+			} else {
+				alert("选错了！");
+				if (score > 0) {
+					score -= constant.timeScore;
+				}
+
+				setTimeout(function() {
+					var t1 = show(correctWord);
+					var t2 = show(_getWrongWord(correctWord));
+					var t3 = show(_getWrongWord(correctWord));
+				}, 300);
+			}
+		}
+
+		
+		// 设置id=score的标签的文本内容(在这里指的是p标签)
+		$('#score').text('得分：' + score);
+		$("body").css('cursor', 'url(../images/mouse_game/cursor-down.ico), default');
+		setTimeout(function() {
+			$("body").trigger("mouseup");
+		}, 250);
+		if (score >= constant.totalScore) {
+			$("#nextGameBtn").show();
+			$("#startBtn").hide();
+			_hideAllHole();
+			clearInterval(time1);
+			alert('游戏结束');
+			return
+		}
+
+		return false;
+	});
+
+	//播放
+	$audioImg.on("click", function() {
+		yinyueDom.play();
+	});
+
+	// 鼠标没有被按下时，指针图片改为image/cursor.ico
+	$('body').mouseup(function() {
+		if (constant.isStart) {
+			$(this).css('cursor', 'url(../images/mouse_game/cursor.ico), default');
+		}
+	});
+
+	$(".close").on("click", function() {
+		location.href = getUrl + "/lessons/unit.html#" + unit;
+	});
+})
