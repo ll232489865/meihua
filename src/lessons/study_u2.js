b5c8e1deed02aaa9a1b155c9c7f35785b0e99799
mangdehuang <zhuerwei3320213>
2017-06-11 18:09:43 +0800

    unit2  audio实现  进度更新实现 Signed-off-by: mangdehuang <zhuerwei3320213>

--- src/lessons/study_u2.js
+++ src/lessons/study_u2.js
@@ -25,7 +25,8 @@ define(['jquery','moduleHtml','powerSwitch','canvas'],function($,template,powerS
     var header = template.getApiKey();
     var unit = template.catchParameter(window.location.href, '#');
     var page = '1';
-    var unitData = {};     
+    var unitData = {};
+    var yingyueDom = document.getElementById("audio1");
 
     $("#ce_position").find("a").powerSwitch({
         eventType: "hover",
@@ -81,7 +82,7 @@ define(['jquery','moduleHtml','powerSwitch','canvas'],function($,template,powerS
 			success: function(data) {				
 				unitData = data.data.componentGroups[0].components;
 				if (unitData.length > 0) {
-					//showVideo(unitData);
+                    updateCash(unitData);
 				}
 			},
 			error: function() {
@@ -89,6 +90,30 @@ define(['jquery','moduleHtml','powerSwitch','canvas'],function($,template,powerS
 			}
 		});
     };
+
+    function updateCash(data) {//更新课程数据
+        var item = {};
+        unitData = [];
+        for(var i =0;i <data.length;i ++){
+            item = data[i].resources;
+            item.readed = false;
+            unitData.push(item);
+        }
+    }
+
+    function  playSong(text) {//根据单词  然后更新缓存中read状态
+        var item = {};
+        for(var key in unitData){
+            item = unitData[key];
+            if(item[0].content == text){
+                item.readed = true;
+                yingyueDom.src =  getUrl + item[1].content;
+                yingyueDom.load();
+                yingyueDom.play();
+                return;
+            }
+        }
+    }
 
 
     function _show($dom, flag) {
@@ -106,22 +131,44 @@ define(['jquery','moduleHtml','powerSwitch','canvas'],function($,template,powerS
 		}
 	}
 
+    function  updateProgress() {//更新课程进度
+        //判断是否需要更新
+        for(var i =0;i<unitData.length;i++){
+            if(!unitData[i].readed) return;
+        }
+        //ajax更新
+        updateGrade();
+    }
+
+    function updateGrade() {
+        var param= {};
+        param.data = {"unit": parseInt(unit),"part": "study","page": parseInt(page),"grade": 0};
+        param.getUrl = getUrl;
+        param.header = header;
+        template.updateGrade(param);
+    }
+
 	//点击事件
 	$(".pic_content").on("click",function(){ 
 		_show($(this),true);
 		if($(this).hasClass('pic1_img')){
             _show($(".dialog_1"),true);
             $(".dialog_1").addClass("readed");
+            playSong("Fine");
 		} else if($(this).hasClass('pic2_img')){
             _show($(".dialog_2"),true);
             $(".dialog_2").addClass("readed");
+            playSong("Hi, how are you?");
         }else if($(this).hasClass('pic3_img')){
             _show($(".dialog_3"),true);
             $(".dialog_3").addClass("readed");
+            playSong("Thank you!");
         }else if($(this).hasClass('pic4_img')){
             _show($(".dialog_4"),true);
             $(".dialog_4").addClass("readed");
+            playSong("Here you are.");
         }
+        updateProgress();
 	});
 
 
